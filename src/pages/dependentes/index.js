23c5315995680e3d438f25ffefdb8f1a02dc3011
Mateuskrp <kronbauer62@gmail.com>
2024-02-01 14:07:08 -0300

    Atualização de Dependentes

--- src/pages/dependentes/index.js
+++ src/pages/dependentes/index.js
@@ -3,8 +3,26 @@ import Header from '../../components/header/header';
 import './dependentes.css'
 import DateMaskInput from '../../components/inputs';
 import Switch from '@mui/material/Switch';
-
+import PetsIcon from '@mui/icons-material/Pets';
+import PersonIcon from '@mui/icons-material/Person';
 const label = { inputProps: { 'aria-label': 'Switch demo' } };
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import ModeEditOutlineIcon from '@mui/icons-material/ModeEditOutline';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+
+function createData(name, filiacao, carencia, falecimento, valor, especie) {
+    return { name, filiacao, carencia, falecimento, valor, especie };
+}
+
+const rows = [
+    createData('Tor', '15/01/2023', '15/01/2025', '00/00/0000', '100,00', "Gator"),
+];
 
 const Dependentes = () => {
     const [mostrarFormularioPet, setMostrarFormularioPet] = useState(false);
@@ -25,18 +43,112 @@ const Dependentes = () => {
                             className={mostrarFormularioPet ? '' : 'botao-ativo'}
                             onClick={() => mostrarFormulario('pet')}
                         >
-                            PET
+                            <PetsIcon fontSize={'small'} />  PET
                         </button>
                         <button
                             className={mostrarFormularioCremacao ? '' : 'botao-ativo'}
                             onClick={() => mostrarFormulario('cremacao')}
                         >
-                            CREMAÇÃO HUMANA
+                            <PersonIcon fontSize={'small'} /> CREMAÇÃO HUMANA
                         </button>
                     </div>
                     {mostrarFormularioPet && (
                         <div>
-                            <h1>teste</h1>
+                            <div className='layout-linha'>
+                                <div className='container-linha'>
+                                    <div className='campos-01'>
+                                        <label>Nome</label>
+                                        <input></input>
+                                    </div>
+                                    <div className='campos-02'>
+                                        <label>Data Nascimento</label>
+                                        <DateMaskInput />
+                                    </div>
+                                    <div className='campos-03'>
+                                        <label>Data Filiação</label>
+                                        <DateMaskInput />
+                                    </div>
+                                    <div className='campos-03'>
+                                        <label>Peso</label>
+                                        <input></input>
+                                    </div>
+                                    <div className='campos-03'>
+                                        <label>Altura</label>
+                                        <input></input>
+                                    </div>
+                                    <div className='campos-03'>
+                                        <label>Cor</label>
+                                        <input></input>
+                                    </div>
+
+                                </div>
+                                <div className='container-linha'>
+                                    <div className='campos-02'>
+                                        <label>Espécie</label>
+                                        <select></select>
+                                    </div>
+                                    <div className='campos-02'>
+                                        <label>Raça</label>
+                                        <select></select>
+                                    </div>
+                                    <div className='campos-02'>
+                                        <label>Porte</label>
+                                        <select></select>
+                                    </div>
+                                    <div className='campos-02'>
+                                        <label>Modalidade</label>
+                                        <select></select>
+                                    </div>
+                                    <div className='salva-dependentes'>
+                                        <button>SALVAR</button>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className='container-linha2'>
+                                <TableContainer component={Paper}>
+                                    <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                                        <TableHead>
+                                            <TableRow>
+                                                <TableCell>Nome</TableCell>
+                                                <TableCell align="center">Filiação</TableCell>
+                                                <TableCell align="center">Fim Carência</TableCell>
+                                                <TableCell align="center">Falecimento</TableCell>
+                                                <TableCell align="center">Valor</TableCell>
+                                                <TableCell align="center">Espécie</TableCell>
+                                                <TableCell align="center">Opções</TableCell>
+                                            </TableRow>
+                                        </TableHead>
+                                        <TableBody>
+                                            {rows.map((row) => (
+                                                <TableRow
+                                                    key={row.name}
+                                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                                >
+                                                    <TableCell component="th" scope="row">
+                                                        {row.name}
+                                                    </TableCell>
+                                                    <TableCell align="center">{row.filiacao}</TableCell>
+                                                    <TableCell align="center">{row.carencia}</TableCell>
+                                                    <TableCell align="center">{row.falecimento}</TableCell>
+                                                    <TableCell align="center">{row.valor}</TableCell>
+                                                    <TableCell align="center">{row.especie}</TableCell>
+                                                    <TableCell align="center">
+                                                        <div className='botao-opcao'>
+                                                            <div className='edit-botao'>
+                                                                <button><ModeEditOutlineIcon fontSize={'small'} /></button>
+                                                            </div>
+                                                            <div className='delete-botao'>
+                                                                <button><DeleteForeverIcon fontSize={'small'} /></button>
+                                                            </div>
+                                                        </div>
+                                                    </TableCell>
+                                                </TableRow>
+                                            ))}
+                                        </TableBody>
+                                    </Table>
+                                </TableContainer>
+                            </div>
                         </div>
                     )}
                     {mostrarFormularioCremacao && (
@@ -83,6 +195,10 @@ const Dependentes = () => {
                                             <label>DEPENDENTE</label>
                                         </div>
                                     </div>
+                                    <div className='salva-dependentes'>
+                                        <button>SALVAR</button>
+                                    </div>
+
                                 </div>
                                 <div className='container-linha'>
                                     <div className='campos-legenda'>
@@ -100,6 +216,49 @@ const Dependentes = () => {
                                         </div>
                                     </div>
                                 </div>
+
+                            </div>
+                            <div className='container-linha2'>
+                                <TableContainer component={Paper}>
+                                    <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                                        <TableHead>
+                                            <TableRow>
+                                                <TableCell>Nome</TableCell>
+                                                <TableCell align="center">Filiação</TableCell>
+                                                <TableCell align="center">Fim Carência</TableCell>
+                                                <TableCell align="center">Falecimento</TableCell>
+                                                <TableCell align="center">Valor</TableCell>
+                                                <TableCell align="center">Opções</TableCell>
+                                            </TableRow>
+                                        </TableHead>
+                                        <TableBody>
+                                            {rows.map((row) => (
+                                                <TableRow
+                                                    key={row.name}
+                                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                                >
+                                                    <TableCell component="th" scope="row">
+                                                        {row.name}
+                                                    </TableCell>
+                                                    <TableCell align="center">{row.filiacao}</TableCell>
+                                                    <TableCell align="center">{row.carencia}</TableCell>
+                                                    <TableCell align="center">{row.falecimento}</TableCell>
+                                                    <TableCell align="center">{row.valor}</TableCell>
+                                                    <TableCell align="center">
+                                                        <div className='botao-opcao'>
+                                                            <div className='edit-botao'>
+                                                                <button><ModeEditOutlineIcon fontSize={'small'} /></button>
+                                                            </div>
+                                                            <div className='delete-botao'>
+                                                                <button><DeleteForeverIcon fontSize={'small'} /></button>
+                                                            </div>
+                                                        </div>
+                                                    </TableCell>
+                                                </TableRow>
+                                            ))}
+                                        </TableBody>
+                                    </Table>
+                                </TableContainer>
                             </div>
                         </div>
                     )}
